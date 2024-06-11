91aff864b5199f32a13efebeb676810cec78b361
Emma Åstrand <aastrand.emma@gmail.com>
2024-06-11 09:34:37 +0200

    Initial commit

---
+++ src/main/java/org/github/aastrandemma/todoapi/TodoApiApplication.java
@@ -0,0 +1,13 @@
+package org.github.aastrandemma.todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApiApplication.class, args);
+    }
+
+}
