3218a24126c86bfb83d1dc58055e918a2c0be896
Emma Åstrand <aastrand.emma@gmail.com>
2024-06-13 16:29:07 +0200

    feat: add PasswordEncoder

---
+++ src/main/java/org/github/aastrandemma/todoapi/util/CustomPasswordEncoder.java
@@ -0,0 +1,20 @@
+package org.github.aastrandemma.todoapi.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomPasswordEncoder implements PasswordEncoder {
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
